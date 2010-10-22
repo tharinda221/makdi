7f72c782054b6db0e5f5c359b1b0746355536cd6
jha.rajeev <jha.rajeev@396820ba-59ff-3b58-1749-98bdfa7c18bb>
2010-10-22 09:49:14 +0000

    code changes

--- src/webgloo/makdi/processor/GoogleHotTrendProcessor.java
+++ src/webgloo/makdi/processor/GoogleHotTrendProcessor.java
@@ -1,11 +1,11 @@
-
 package webgloo.makdi.processor;
 
-import java.util.ArrayList;
 import java.util.List;
 import org.apache.commons.lang.WordUtils;
 import webgloo.makdi.data.IData;
 import webgloo.makdi.data.Keyword;
+import webgloo.makdi.db.DBConnection;
+import webgloo.makdi.db.DBManager;
 import webgloo.makdi.drivers.IDriver;
 import webgloo.makdi.profile.IProfileBean;
 import webgloo.makdi.util.MyWriter;
@@ -16,55 +16,57 @@ import webgloo.makdi.util.MyWriter;
  */
 public class GoogleHotTrendProcessor {
 
-    public static void invoke(IProfileBean profileBean) throws Exception{
-         //first refresh the keywords
+    public static void invoke(IProfileBean profileBean) throws Exception {
+        //first refresh the keywords
         List<Keyword> newKeywords = GoogleHotTrendKeywords.loadNewKeywords();
-        //java.sql.Connection connection = DBConnection.getConnection();
-        //DBManager.storeKeywords(connection, GoogleHotTrendKeywords.loadNewKeywords());
-
-        //Now find unprocessed keywords
-        //List<String> keywords = DBManager.loadKeywords(connection, profile.getSiteGuid());
-        List<String> keywords = new ArrayList<String>();
-        for(Keyword k : newKeywords) {
-            keywords.add(k.getToken());
-
-        }
-
+        java.sql.Connection connection = DBConnection.getConnection();
+        //plug in logic to remove duplicate keywords 
         List<IDriver> drivers = profileBean.getDrivers();
 
-        for (String keyword : keywords) {
+        for (Keyword keyword : newKeywords) {
 
             MyWriter.toConsole("\n** start process :: " + keyword);
-            keyword = WordUtils.capitalizeFully(keyword);
+            String token = WordUtils.capitalizeFully(keyword.getToken());
             //String pageId = MyStringUtil.convertPageNameToId(keyword);
             StringBuilder content = new StringBuilder();
             StringBuilder summary = new StringBuilder();
 
+            int count = 0;
+
             //get content
             for (IDriver driver : drivers) {
 
-                List<IData> items = driver.run(keyword);
+                List<IData> items = driver.run(token);
                 for (IData item : items) {
                     content.append(item.toHtml());
                 }
             }
-            
+
             //get summary
-            List<IData> items = profileBean.getFrontPageDriver().run(keyword);
+            List<IData> items = profileBean.getFrontPageDriver().run(token);
             for (IData item : items) {
                 summary.append(item.toHtml());
+                count++;
             }
 
             //store summary and  content
+            if (count > 0) {
+                //store content for this keyword
+                DBManager.storePageContent(connection,
+                        profileBean.getSiteGuid(),
+                        token,
+                        " automated ocontent for " + token,
+                        summary.toString(),
+                        content.toString());
+
+            }
+
             MyWriter.toConsole("\n** summary :: " + summary.toString());
-            MyWriter.toConsole("\n** content :: " + content.toString());
             MyWriter.toConsole("\n** end process :: " + keyword);
-            break ;
 
         } //loop: keywords
 
-
-        //connection.close();
+        connection.close();
 
     }
 }
